ba22e853527b77b532edf9bb14491be3752be5c0
andrey <an.isaev7@gmail.com>
2022-11-02 00:08:52 +0300

    step 5 ready

--- app/users_blueprint.py
+++ app/users_blueprint.py
@@ -1,8 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from app.models import Users
 
 user_blueprint = Blueprint("user_blueprint", __name__)
 
 @user_blueprint.route("/users")
-def all_users():
-    result = []
-    users = User.qe
+def get_all_users():
+    res = []
+    users = Users.query.all()
+    for user in users:
+        res.append(user.to_dict())
+    return jsonify(res)
+
+
+@user_blueprint.route("/users/<int:pk>")
+def users_by_pk(pk):
+    user = Users.query.get(pk)
+    return jsonify(user.to_dict())
--- utils.py
+++ utils.py
@@ -1,46 +1,48 @@
-# def init_database():
-#     import app
-#     import models
-#     import data
-#     app.db.drop_all()
-#     app.db.create_all()
-#
-#     for user in data.users:
-#         app.db.session.add(
-#             models.Users(
-#                 id=user.get("id"),
-#                 first_name=user.get("first_name"),
-#                 last_name=user.get("last_name"),
-#                 age=user.get("age"),
-#                 email=user.get("email"),
-#                 role=user.get("role"),
-#                 phone=user.get("phone"),
-#             )
-#         )
-#         app.db.session.commit()
-#
-#     for order in data.orders:
-#         app.db.session.add(
-#             models.Orders(
-#                 id=order.get("id"),
-#                 name=order.get("name"),
-#                 description=order.get("description"),
-#                 start_date=order.get("start_date"),
-#                 end_date=order.get("end_date"),
-#                 address=order.get("address"),
-#                 price=order.get("price"),
-#                 customer_id=order.get("customer_id"),
-#                 executor_id=order.get("executor_id"),
-#             )
-#         )
-#         app.db.session.commit()
-#
-#     for offer in data.offers:
-#         app.db.session.add(
-#             models.Offers(
-#                 id=offer.get("id"),
-#                 order_id=offer.get("order_id"),
-#                 executor_id=offer.get("executor_id"),
-#             )
-#         )
-#         app.db.session.commit()
+import data
+from app import db
+import app.models as models
+
+
+def init_database():
+    db.drop_all()
+    db.create_all()
+
+    for user in data.users:
+        db.session.add(
+            models.Users(
+                id=user.get("id"),
+                first_name=user.get("first_name"),
+                last_name=user.get("last_name"),
+                age=user.get("age"),
+                email=user.get("email"),
+                role=user.get("role"),
+                phone=user.get("phone"),
+            )
+        )
+        db.session.commit()
+
+    for order in data.orders:
+        db.session.add(
+            models.Orders(
+                id=order.get("id"),
+                name=order.get("name"),
+                description=order.get("description"),
+                start_date=order.get("start_date"),
+                end_date=order.get("end_date"),
+                address=order.get("address"),
+                price=order.get("price"),
+                customer_id=order.get("customer_id"),
+                executor_id=order.get("executor_id"),
+            )
+        )
+        db.session.commit()
+
+    for offer in data.offers:
+        db.session.add(
+            models.Offers(
+                id=offer.get("id"),
+                order_id=offer.get("order_id"),
+                executor_id=offer.get("executor_id"),
+            )
+        )
+        db.session.commit()
