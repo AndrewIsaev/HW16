439f56d233f6c91e255465a7a0cd8ce2c5eb66c8
Andrey Isaev <an.isaev7@gmail.com>
2022-11-02 22:18:10 +0300

    step8

--- app/offers/views.py
+++ app/offers/views.py
@@ -1,19 +1,53 @@
-from flask import Blueprint, jsonify
+import json
+
+from flask import Blueprint, jsonify, request
+
+from app import db
 from app.models import Offers
 
 offer_blueprint = Blueprint("offer_blueprint", __name__)
 
 
-@offer_blueprint.route("/offers")
+@offer_blueprint.route("/offers", methods=["GET", "POST"])
 def get_all_offers():
-    res = []
-    offers = Offers.query.all()
-    for offer in offers:
-        res.append(offer.to_dict())
-    return jsonify(res)
+    if request.method == "GET":
+        res = []
+        offers = Offers.query.all()
+        for offer in offers:
+            res.append(offer.to_dict())
+        return jsonify(res)
+
+    if request.method == "POST":
+        offer_data = json.loads(request.data)
+        offer = Offers(
+            id=offer_data.get("id"),
+            order_id=offer_data.get("order_id"),
+            executor_id=offer_data.get("executor_id"),
+        )
+        db.session.add(offer)
+        db.session.commit()
+        return "Offer successfully added!"
 
 
-@offer_blueprint.route("/offers/<int:pk>")
+@offer_blueprint.route("/offers/<int:pk>", methods=["GET", "PUT", "DELETE"])
 def offer_by_pk(pk):
-    offer = Offers.query.get(pk)
-    return jsonify(offer.to_dict())
+    if request.method == "GET":
+        offer = Offers.query.get(pk)
+        return jsonify(offer.to_dict())
+    if request.method == "PUT":
+        offer_data = json.loads(request.data)
+        offer = Offers.query.get(pk)
+
+        offer.id = offer_data.get("id")
+        offer.order_id = offer_data.get("order_id")
+        offer.executor_id = offer_data.get("executor_id")
+
+        db.session.add(offer)
+        db.session.commit()
+        return "Offer successfully updated!"
+
+    if request.method == "DELETE":
+        offer = Offers.query.get(pk)
+        db.session.delete(offer)
+        db.session.commit()
+        return "Offer successfully deleted!"
--- app/orders/views.py
+++ app/orders/views.py
@@ -1,18 +1,64 @@
-from flask import Blueprint, jsonify
+import json
+
+from flask import Blueprint, jsonify, request
+
+from app import db
 from app.models import Orders
 
 order_blueprint = Blueprint("order_blueprint", __name__)
 
-@order_blueprint.route("/orders")
+
+@order_blueprint.route("/orders", methods=["GET", "POST"])
 def get_all_orders():
-    res = []
-    orders = Orders.query.all()
-    for order in orders:
-        res.append(order.to_dict())
-    return jsonify(res)
+    if request.method == "GET":
+        res = []
+        orders = Orders.query.all()
+        for order in orders:
+            res.append(order.to_dict())
+        return jsonify(res)
+
+    if request.method == "POST":
+        order_data = json.loads(request.data)
+        user = Orders(
+            id=order_data.get("id"),
+            name=order_data.get("name"),
+            description=order_data.get("description"),
+            start_date=order_data.get("start_date"),
+            end_date=order_data.get("end_date"),
+            address=order_data.get("address"),
+            price=order_data.get("price"),
+            customer_id=order_data.get("customer_id"),
+            executor_id=order_data.get("executor_id"),
+        )
+        db.session.add(user)
+        db.session.commit()
+        return "Order successfully added!"
 
 
-@order_blueprint.route("/orders/<int:pk>")
+@order_blueprint.route("/orders/<int:pk>", methods=["GET", "PUT", "DELETE"])
 def order_by_pk(pk):
-    order = Orders.query.get(pk)
-    return jsonify(order.to_dict())
+    if request.method == "GET":
+        order = Orders.query.get(pk)
+        return jsonify(order.to_dict())
+    if request.method == "PUT":
+        order_data = json.loads(request.data)
+        order = Orders.query.get(pk)
+
+        order.name = order_data.get("name")
+        order.description = order_data.get("description")
+        order.start_date = order_data.get("start_date")
+        order.end_date = order_data.get("end_date")
+        order.address = order_data.get("address")
+        order.price = order_data.get("price")
+        order.customer_id = order_data.get("customer_id")
+        order.executor_id = order_data.get("executor_id")
+
+        db.session.add(order)
+        db.session.commit()
+        return "User successfully updated!"
+
+    if request.method == "DELETE":
+        order = Orders.query.get(pk)
+        db.session.delete(order)
+        db.session.commit()
+        return "User successfully deleted!"
